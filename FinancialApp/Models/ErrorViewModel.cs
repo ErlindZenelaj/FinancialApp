e85765b570cdb465a2c94cdc6ce28eefc1badb59
Erlind <81335694+ErlindZenelaj@users.noreply.github.com>
2022-12-14 14:43:40 +0100

    Wazzappp

---
+++ FinancialApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
